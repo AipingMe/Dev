39fa4f8a222def7031c2d4f44ba69efe4f336fb2
贺爱平 <aphe@Ctrip.com>
2015-05-18 10:36:59 +0800

    format code

--- Aiping.Condition/Aiping.Condition.Imp/ConditionCheckerOne.cs
+++ Aiping.Condition/Aiping.Condition.Imp/ConditionCheckerOne.cs
@@ -1,13 +1,8 @@
 ﻿using Aiping.Condition.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aiping.Condition.Imp
 {
-    class ConditionCheckerOne : IConditionChecker
+    internal class ConditionCheckerOne : IConditionChecker
     {
         public bool IsValid(int orderId)
         {
--- Aiping.Condition/Aiping.Condition.Imp/IConditionCheckerTwo.cs
+++ Aiping.Condition/Aiping.Condition.Imp/IConditionCheckerTwo.cs
@@ -1,9 +1,5 @@
 ﻿using Aiping.Condition.Interface;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aiping.Condition.Imp
 {
